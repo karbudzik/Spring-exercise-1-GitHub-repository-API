db6a66509b7807a3e188fdee3f747df9cc8a906f
karbudzik <karolinabudzik92@gmail.com>
2020-08-07 00:34:19 +0200

    Date format change

--- src/main/java/com/karolina/githubrepositoryapi/model/Repository.java
+++ src/main/java/com/karolina/githubrepositoryapi/model/Repository.java
@@ -4,19 +4,25 @@ import com.fasterxml.jackson.annotation.JsonSetter;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.TemporalAccessor;
 import java.util.Date;
+import java.util.TimeZone;
 
 @Getter
 @NoArgsConstructor
 public class Repository {
     @JsonSetter("name")
-    private String name;
+    private String fullName;
     @JsonSetter("description")
     private String description;
     @JsonSetter("clone_url")
     private String cloneUrl;
     @JsonSetter("stargazers_count")
-    private int starGazers;
+    private int stars;
     @JsonSetter("created_at")
-    private Date dateOfCreation;
+    private String createdAt;
 }
